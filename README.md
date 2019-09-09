d8ef001925c75d2ab7ee9da8268ac2e41a0297c0
Fabian Hinz <hinzfabian.fh@gmail.com>
2019-09-09 15:20:11 +0200

    init startseite

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # T.B.D
+
+## icon source
+
+- https://www.flaticon.com/packs/restaurant-95
+- https://www.flaticon.com/packs/technology-of-the-future-13
+
+## may wanna use this
+
+- https://www.npmjs.com/package/react-loading-skeleton
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import "typeface-roboto";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
+import { SnackbarProvider } from "notistack";
+import ThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { createMuiTheme } from "@material-ui/core";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#A5D6A7" },
+    secondary: { main: "#FFCC80" }
+  }
+});
+
+ReactDOM.render(
+  <>
+    <CssBaseline />
+    <Router history={createBrowserHistory()}>
+      <ThemeProvider theme={theme}>
+        <SnackbarProvider
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+        >
+          <App />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </Router>
+  </>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
