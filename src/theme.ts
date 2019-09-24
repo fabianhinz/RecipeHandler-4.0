d646f7c48179e367dc62b486a76c348a5ee7dd01
Fabian Hinz <hinzfabian.fh@gmail.com>
2019-09-24 08:56:32 +0200

    MuiAvatar has been fixed, delete that workaround

--- src/theme.ts
+++ src/theme.ts
@@ -55,16 +55,6 @@ const sharedTheme: Partial<ThemeOptions> = {
                 alignSelf: "center"
             }
         }
-        // ! dirty workaround for https://github.com/mui-org/material-ui/issues/16374
-        // MuiAvatar: {
-        //     root: {
-        //         height: 32,
-        //         width: 32
-        //     },
-        //     colorDefault: {
-        //         color: "inherit"
-        //     }
-        // }
     }
 };
 
