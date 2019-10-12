2d0d491c32ab092212e6a2a8b275949cd51b302c
Miriam Alber <44898638+Miwri@users.noreply.github.com>
2019-10-12 21:19:56 +0200

    Feature/algoliasearch (#58)
    * show edit button  on home recipes
    
    * poc algoliasearch client
    
    * start algolia search
    
    * WIP show fragments
    
    * WIP first try to gihlight something
    
    * WIP it gets better
    
    * mein senf
    
    * better types
    
    * algolia branding
    
    * cleanup
    
    * small layout changes

--- src/Components/Home/RecentlyAdded/HomeRecentlyAdded.tsx
+++ src/Components/Home/RecentlyAdded/HomeRecentlyAdded.tsx
@@ -1,47 +1,105 @@
 import {
     Box,
+    CircularProgress,
     createStyles,
+    Divider,
     Drawer,
     Fab,
     Grid,
+    InputAdornment,
     InputBase,
+    List,
+    ListItem,
+    ListItemText,
     makeStyles,
-    Tooltip,
 } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/SearchTwoTone'
 import React, { useEffect, useState } from 'react'
+import Highlighter from 'react-highlight-words'
 
+import { index } from '../../../algolia'
 import { FirebaseService } from '../../../firebase'
 import useDebounce from '../../../hooks/useDebounce'
+import { ReactComponent as AlgoliaIcon } from '../../../icons/algolia.svg'
+import { ReactComponent as NotFoundIcon } from '../../../icons/notFound.svg'
 import { RecipeDocument } from '../../../model/model'
 import { useBreakpointsContext } from '../../Provider/BreakpointsProvider'
+import { useRouterContext } from '../../Provider/RouterProvider'
+import { PATHS } from '../../Routes/Routes'
 import { HomeRecentlyAddedCard } from './HomeRecentlyAddedCard'
 
 const useStyles = makeStyles(() =>
     createStyles({
-        root: {
-            background: 'none',
+        list: {
+            maxHeight: '100%',
+            overflowY: 'auto',
         },
     })
 )
+
+type Hit = Pick<RecipeDocument, 'name' | 'description' | 'ingredients'> & {
+    _highlightResult: {
+        name: { value: string }
+        description: { value: string }
+        ingredients: { value: string }
+    }
+}
+
+type Hits = Array<Hit>
+
+const getHighlightedDescription = (description: string) => {
+    const descriptionArray = description.split(/1\.|- |\*{1,}|#{1,}/gi)
+    const displayResults: Array<string> = []
+    const displayResultsNice: Array<string> = []
+
+    descriptionArray.forEach(step => {
+        if (step.indexOf('<em>') !== -1) {
+            displayResults.push('...')
+            displayResults.push(step)
+        }
+    })
+
+    displayResults.forEach(recipeFragment => {
+        displayResultsNice.push(recipeFragment.replace(/<\/?em>/gi, ''))
+    })
+
+    return displayResultsNice
+}
+
+const getHighlightedIngredients = (ingredients: string) => {
+    const ingredientsArray = ingredients.split('- ')
+
+    const displayResults: Array<string> = []
+    const displayResultsNice: Array<string> = []
+
+    ingredientsArray.forEach(ingredient => {
+        if (ingredient.indexOf('<em>') !== -1) {
+            displayResults.push('...')
+            displayResults.push(ingredient)
+        }
+    })
+
+    displayResults.forEach(recipeFragment => {
+        displayResultsNice.push(recipeFragment.replace(/<\/?em>/gi, ''))
+    })
+
+    return displayResultsNice
+}
 
 export const HomeRecentlyAdded = () => {
     const [recipes, setRecipes] = useState<Array<RecipeDocument>>([])
     const [searchDrawer, setSearchDrawer] = useState(false)
     const [searchValue, setSearchValue] = useState('')
-    const [skeleton, setSkeleton] = useState(false)
+    const [algoliaHits, setAlgoliaHits] = useState<Hits>([])
+    const [loading, setLoading] = useState(false)
     const { isMobile } = useBreakpointsContext()
-
-    const debouncedSearchValue = useDebounce(searchValue, 500)
 
     const classes = useStyles()
 
-    const handleSearchDrawerChange = () => setSearchDrawer(previous => !previous)
+    const debouncedSearchValue = useDebounce(searchValue, 500)
+    const { history } = useRouterContext()
 
-    useEffect(() => {
-        if (searchValue !== debouncedSearchValue) setSkeleton(true)
-        else setSkeleton(false)
-    }, [debouncedSearchValue, searchValue])
+    const handleSearchDrawerChange = () => setSearchDrawer(previous => !previous)
 
     useEffect(() => {
         let query:
@@ -51,69 +109,121 @@ export const HomeRecentlyAdded = () => {
         const limit = isMobile ? 3 : 6
 
         if (debouncedSearchValue.length > 0) {
-            return query
-                .where('name', '>=', debouncedSearchValue)
-                .limit(limit)
-                .onSnapshot(
-                    querySnapshot => {
-                        setRecipes(querySnapshot.docs.map(doc => doc.data() as RecipeDocument))
-                        setSkeleton(false)
-                    },
-                    error => console.error(error)
-                )
+            index.search(debouncedSearchValue).then(({ hits }) => {
+                setAlgoliaHits(hits)
+                setLoading(false)
+            })
         } else {
-            return query
-                .orderBy('createdDate', 'desc')
-                .limit(limit)
-                .onSnapshot(
-                    querySnapshot => {
-                        setRecipes(querySnapshot.docs.map(doc => doc.data() as RecipeDocument))
-                    },
-                    error => console.error(error)
-                )
+            setAlgoliaHits([])
+            setLoading(false)
         }
+
+        return query
+            .orderBy('createdDate', 'desc')
+            .limit(limit)
+            .onSnapshot(
+                querySnapshot => {
+                    setRecipes(querySnapshot.docs.map(doc => doc.data() as RecipeDocument))
+                },
+                error => console.error(error)
+            )
     }, [debouncedSearchValue, isMobile])
 
     return (
         <>
             <Box marginBottom={2}>
                 <Box marginBottom={2} display="flex" justifyContent="space-evenly">
-                    <Tooltip title="Kürzlich hinzugefügte einschränken">
-                        <div>
-                            <Fab onClick={handleSearchDrawerChange} size="small" color="primary">
-                                <SearchIcon />
-                            </Fab>
-                        </div>
-                    </Tooltip>
+                    <Fab onClick={handleSearchDrawerChange} size="small" color="primary">
+                        <SearchIcon />
+                    </Fab>
                 </Box>
                 <Grid container spacing={2}>
                     {recipes.map(recipe => (
-                        <HomeRecentlyAddedCard
-                            skeleton={skeleton}
-                            key={recipe.name}
-                            recipe={recipe}
-                        />
+                        <HomeRecentlyAddedCard skeleton={false} key={recipe.name} recipe={recipe} />
                     ))}
                 </Grid>
             </Box>
 
-            <Drawer
-                BackdropProps={{ classes }}
-                open={searchDrawer}
-                onClose={handleSearchDrawerChange}
-                anchor="top">
-                <Box padding={2} display="flex" alignItems="center">
-                    <Box marginRight={1}>
-                        <SearchIcon />
-                    </Box>
+            <Drawer open={searchDrawer} onClose={handleSearchDrawerChange} anchor="top">
+                <Box padding={2}>
                     <InputBase
                         autoFocus
                         fullWidth
-                        placeholder="Nach Rezeptnamen suchen"
+                        placeholder="Rezepte durchsuchen"
                         value={searchValue}
-                        onChange={e => setSearchValue(e.target.value)}
+                        onChange={e => {
+                            setLoading(true)
+                            setSearchValue(e.target.value)
+                        }}
+                        startAdornment={
+                            <InputAdornment position="start">
+                                {loading ? (
+                                    <CircularProgress thickness={8} size={24} />
+                                ) : (
+                                    <SearchIcon />
+                                )}
+                            </InputAdornment>
+                        }
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <a
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    href="https://www.algolia.com/docsearch">
+                                    <AlgoliaIcon />
+                                </a>
+                            </InputAdornment>
+                        }
                     />
                 </Box>
+
+                <Divider />
+
+                {algoliaHits.length > 0 && (
+                    <List className={classes.list}>
+                        {algoliaHits.map(recipeHit => (
+                            <ListItem
+                                button
+                                onClick={() => history.push(PATHS.details(recipeHit.name))}
+                                key={recipeHit.name}>
+                                <ListItemText
+                                    primary={recipeHit.name}
+                                    secondary={
+                                        <>
+                                            <b>Zutaten:</b>{' '}
+                                            {getHighlightedIngredients(
+                                                recipeHit._highlightResult.ingredients.value
+                                            ).map((recipeFragment, index) => (
+                                                <Highlighter
+                                                    searchWords={[debouncedSearchValue]}
+                                                    textToHighlight={recipeFragment}
+                                                    key={index}
+                                                />
+                                            ))}
+                                            <br />
+                                            <b>Beschreibung:</b>{' '}
+                                            {getHighlightedDescription(
+                                                recipeHit._highlightResult.description.value
+                                            ).map((recipeFragment, index) => (
+                                                <Highlighter
+                                                    searchWords={[debouncedSearchValue]}
+                                                    textToHighlight={recipeFragment}
+                                                    key={index}
+                                                />
+                                            ))}
+                                        </>
+                                    }
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
+                )}
+
+                {!loading && algoliaHits.length === 0 && searchValue.length > 0 && (
+                    <Box padding={2} display="flex" justifyContent="center">
+                        <NotFoundIcon width={150} />
+                    </Box>
+                )}
             </Drawer>
         </>
     )
