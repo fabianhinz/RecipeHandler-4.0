31918bd8a6f92841dcf5411cb0b6841f5b160946
Fabian Hinz <fabianhinz@users.noreply.github.com>
2020-01-07 21:00:30 +0100

    sort recipes via name and createdDate fields (#152)

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,27 +2,28 @@ import AddIcon from '@material-ui/icons/Add'
 import React, { useEffect, useState } from 'react'
 
 import { useCategorySelect } from '../../hooks/useCategorySelect'
-import { DocumentId, RecipeDocument } from '../../model/model'
+import { AttachmentMetadata, DocumentId, Recipe, RecipeDocument } from '../../model/model'
 import { FirebaseService } from '../../services/firebase'
 import { useFirebaseAuthContext } from '../Provider/FirebaseAuthProvider'
 import RecentlyAdded from '../RecentlyAdded/RecentlyAdded'
 import { NavigateFab } from '../Routes/Navigate'
 import { PATHS } from '../Routes/Routes'
 import { HomeCategory } from './HomeCategory'
-import { HomeRecipe } from './HomeRecipe'
+import { HomeRecipe, OrderByKey, OrderByRecord } from './HomeRecipe'
 
 type ChangesRecord = Record<firebase.firestore.DocumentChangeType, Map<DocumentId, RecipeDocument>>
 
 const Home = () => {
     const [pagedRecipes, setPagedRecipes] = useState<Map<DocumentId, RecipeDocument>>(new Map())
-    const [lastRecipeName, setLastRecipeName] = useState('')
+    const [lastRecipe, setLastRecipe] = useState<Recipe<AttachmentMetadata> | null>(null)
     const [loading, setLoading] = useState(true)
+    const [orderBy, setOrderBy] = useState<OrderByRecord>({ name: 'asc' })
 
     const { selectedCategories, setSelectedCategories } = useCategorySelect()
     const { user } = useFirebaseAuthContext()
 
     const handleCategoryChange = (type: string, value: string) => {
-        setLastRecipeName('')
+        setLastRecipe(null)
         setSelectedCategories(type, value)
     }
 
@@ -33,7 +34,7 @@ const Home = () => {
         const observer = new IntersectionObserver(entries => {
             const [lastRecipeTrigger] = entries
             if (lastRecipeTrigger.isIntersecting && pagedRecipes.size > 0)
-                setLastRecipeName([...pagedRecipes.values()].pop()!.name)
+                setLastRecipe([...pagedRecipes.values()].pop()!)
         })
         observer.observe(trigger)
 
@@ -42,57 +43,69 @@ const Home = () => {
 
     useEffect(() => {
         setPagedRecipes(new Map())
-        setLastRecipeName('')
-    }, [user])
+        setLastRecipe(null)
+    }, [user, orderBy])
 
     useEffect(() => {
+        if (selectedCategories.size > 0) return
+        // ! do not merge those to effect hooks
         setLoading(true)
-        // ? constructing the query with both where and orderBy clauses requires multiple indexes
+        const orderByKey = Object.keys(orderBy)[0] as OrderByKey
         let query:
             | firebase.firestore.CollectionReference
-            | firebase.firestore.Query = FirebaseService.firestore.collection('recipes')
+            | firebase.firestore.Query = FirebaseService.firestore
+            .collection('recipes')
+            .orderBy(orderByKey, orderBy[orderByKey])
 
         if (user && user.selectedUsers.length > 0)
             query = query.where('editorUid', 'in', user.selectedUsers)
 
-        if (selectedCategories.size === 0) {
-            return query
-                .orderBy('name', 'asc')
-                .startAfter(lastRecipeName)
-                .limit(8)
-                .onSnapshot(querySnapshot => {
-                    const changes: ChangesRecord = {
-                        added: new Map(),
-                        modified: new Map(),
-                        removed: new Map(),
-                    }
+        if (lastRecipe) query = query.startAfter(lastRecipe[orderByKey])
 
-                    querySnapshot
-                        .docChanges()
-                        .forEach(({ type, doc }) =>
-                            changes[type].set(doc.id, doc.data() as RecipeDocument)
-                        )
-                    setPagedRecipes(recipes => {
-                        changes.removed.forEach((_v, key) => recipes.delete(key))
-                        return new Map([...recipes, ...changes.added, ...changes.modified])
-                    })
-                    setLoading(false)
-                })
-        } else {
-            selectedCategories.forEach(
-                (value, type) => (query = query.where(`categories.${type}`, '==', value))
-            )
-            return query.onSnapshot(
-                querySnapshot => {
-                    const added: Map<DocumentId, RecipeDocument> = new Map()
-                    querySnapshot.docs.map(doc => added.set(doc.id, doc.data() as RecipeDocument))
-                    setPagedRecipes(added)
-                    setLoading(false)
-                },
-                error => console.error(error)
-            )
-        }
-    }, [lastRecipeName, selectedCategories, user])
+        return query.limit(8).onSnapshot(querySnapshot => {
+            const changes: ChangesRecord = {
+                added: new Map(),
+                modified: new Map(),
+                removed: new Map(),
+            }
+            querySnapshot
+                .docChanges()
+                .forEach(({ type, doc }) => changes[type].set(doc.id, doc.data() as RecipeDocument))
+            setPagedRecipes(recipes => {
+                changes.removed.forEach((_v, key) => recipes.delete(key))
+                return new Map([...recipes, ...changes.added, ...changes.modified])
+            })
+            setLoading(false)
+        })
+    }, [lastRecipe, orderBy, selectedCategories, user])
+
+    useEffect(() => {
+        if (selectedCategories.size === 0) return
+        // ! do not merge those to effect hooks
+        setLoading(true)
+        const orderByKey = Object.keys(orderBy)[0] as OrderByKey
+        let query:
+            | firebase.firestore.CollectionReference
+            | firebase.firestore.Query = FirebaseService.firestore
+            .collection('recipes')
+            .orderBy(orderByKey, orderBy[orderByKey])
+
+        if (user && user.selectedUsers.length > 0)
+            query = query.where('editorUid', 'in', user.selectedUsers)
+
+        selectedCategories.forEach(
+            (value, type) => (query = query.where(`categories.${type}`, '==', value))
+        )
+        return query.onSnapshot(
+            querySnapshot => {
+                const added: Map<DocumentId, RecipeDocument> = new Map()
+                querySnapshot.docs.map(doc => added.set(doc.id, doc.data() as RecipeDocument))
+                setPagedRecipes(added)
+                setLoading(false)
+            },
+            error => console.error(error)
+        )
+    }, [orderBy, selectedCategories, user])
 
     return (
         <>
@@ -102,7 +115,12 @@ const Home = () => {
                 selectedCategories={selectedCategories}
                 onCategoryChange={handleCategoryChange}
             />
-            <HomeRecipe skeletons={loading} recipes={[...pagedRecipes.values()]} />
+            <HomeRecipe
+                orderBy={orderBy}
+                onOrderByChange={setOrderBy}
+                skeletons={loading}
+                recipes={[...pagedRecipes.values()]}
+            />
             <div id="intersection-observer-trigger" />
 
             <NavigateFab to={PATHS.recipeCreate} icon={<AddIcon />} />
--- src/Components/Home/HomeRecipe.tsx
+++ src/Components/Home/HomeRecipe.tsx
@@ -1,58 +1,132 @@
-import { Box, Card, CardContent, Grid, Grow } from '@material-ui/core'
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Card,
+    CardActions,
+    CardContent,
+    createStyles,
+    Grid,
+    Grow,
+    makeStyles,
+    Zoom,
+} from '@material-ui/core'
+import DescIcon from '@material-ui/icons/ArrowDownwardRounded'
+import AscIcon from '@material-ui/icons/ArrowUpwardRounded'
 import { Skeleton } from '@material-ui/lab'
 import React from 'react'
 
 import { ReactComponent as NotFoundIcon } from '../../icons/notFound.svg'
 import { AttachmentMetadata, Recipe } from '../../model/model'
+import { useBreakpointsContext } from '../Provider/BreakpointsProvider'
 import RecipeResult from '../Recipe/Result/RecipeResult'
+
+export type OrderByKey = keyof Pick<Recipe<AttachmentMetadata>, 'name' | 'createdDate'>
+export type OrderByRecord = Partial<Record<OrderByKey, 'asc' | 'desc'>>
 
 interface HomeRecipeProps {
     recipes: Array<Recipe<AttachmentMetadata>>
     skeletons: boolean
+    orderBy: OrderByRecord
+    onOrderByChange: (orderBy: OrderByRecord) => void
 }
 
-export const HomeRecipe = ({ recipes, skeletons }: HomeRecipeProps) => (
-    <Box marginBottom={2}>
-        <Card>
-            <CardContent>
-                <Grid container spacing={2}>
-                    {recipes.map(recipe => (
-                        <Grid xs={12} item key={recipe.name}>
-                            <RecipeResult
-                                variant="summary"
-                                recipe={recipe}
-                                divider={recipe.name !== recipes[recipes.length - 1].name}
-                            />
-                        </Grid>
-                    ))}
+const useStyles = makeStyles(() =>
+    createStyles({
+        groupButton: {
+            '&:not(:first-child), &:not(:last-child)': {
+                borderRight: 'none',
+                borderBottom: 'none',
+            },
+        },
+    })
+)
 
-                    {skeletons &&
-                        recipes.length === 0 &&
-                        new Array(8).fill(1).map((_skeleton, index) => (
-                            <Grid xs={12} item key={index}>
-                                <Grid
-                                    container
-                                    spacing={2}
-                                    justify="space-between"
-                                    alignItems="center">
-                                    <Grid xs={12} item>
-                                        <Skeleton width="100%" height={125} variant="rect" />
-                                    </Grid>
-                                </Grid>
+export const HomeRecipe = ({ recipes, skeletons, orderBy, onOrderByChange }: HomeRecipeProps) => {
+    const { isDialogFullscreen } = useBreakpointsContext()
+    const classes = useStyles()
+
+    const handleOrderByChange = (key: keyof OrderByRecord) => () => {
+        if (orderBy[key] && orderBy[key] === 'asc') onOrderByChange({ [key]: 'desc' })
+        else if (orderBy[key] && orderBy[key] === 'desc') onOrderByChange({ [key]: 'asc' })
+        else onOrderByChange({ [key]: 'asc' })
+    }
+
+    const getStartIcon = (orderBy?: 'asc' | 'desc') => {
+        if (!orderBy) return {}
+
+        return {
+            startIcon: <Zoom in>{orderBy === 'asc' ? <AscIcon /> : <DescIcon />}</Zoom>,
+        }
+    }
+
+    return (
+        <Box marginBottom={2}>
+            <Card>
+                <CardActions>
+                    <Box display="flex" justifyContent="center" flexGrow={1}>
+                        <ButtonGroup
+                            classes={{
+                                groupedTextHorizontal: classes.groupButton,
+                                groupedTextVertical: classes.groupButton,
+                            }}
+                            orientation={isDialogFullscreen ? 'vertical' : 'horizontal'}
+                            variant="contained">
+                            <Button
+                                onClick={handleOrderByChange('name')}
+                                color={orderBy.name ? 'primary' : 'default'}
+                                {...getStartIcon(orderBy.name)}>
+                                Name
+                            </Button>
+                            <Button
+                                onClick={handleOrderByChange('createdDate')}
+                                color={orderBy.createdDate ? 'primary' : 'default'}
+                                {...getStartIcon(orderBy.createdDate)}>
+                                Datum
+                            </Button>
+                        </ButtonGroup>
+                    </Box>
+                </CardActions>
+                <CardContent>
+                    <Grid container spacing={2}>
+                        {recipes.map(recipe => (
+                            <Grid xs={12} item key={recipe.name}>
+                                <RecipeResult
+                                    variant="summary"
+                                    recipe={recipe}
+                                    divider={recipe.name !== recipes[recipes.length - 1].name}
+                                />
                             </Grid>
                         ))}
 
-                    {!skeletons && recipes.length === 0 && (
-                        <Grid item xs={12}>
-                            <Box display="flex" justifyContent="center">
-                                <Grow in timeout={500}>
-                                    <NotFoundIcon width={200} />
-                                </Grow>
-                            </Box>
-                        </Grid>
-                    )}
-                </Grid>
-            </CardContent>
-        </Card>
-    </Box>
-)
+                        {skeletons &&
+                            recipes.length === 0 &&
+                            new Array(8).fill(1).map((_skeleton, index) => (
+                                <Grid xs={12} item key={index}>
+                                    <Grid
+                                        container
+                                        spacing={2}
+                                        justify="space-between"
+                                        alignItems="center">
+                                        <Grid xs={12} item>
+                                            <Skeleton width="100%" height={125} variant="rect" />
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            ))}
+
+                        {!skeletons && recipes.length === 0 && (
+                            <Grid item xs={12}>
+                                <Box display="flex" justifyContent="center">
+                                    <Grow in timeout={500}>
+                                        <NotFoundIcon width={200} />
+                                    </Grow>
+                                </Box>
+                            </Grid>
+                        )}
+                    </Grid>
+                </CardContent>
+            </Card>
+        </Box>
+    )
+}
