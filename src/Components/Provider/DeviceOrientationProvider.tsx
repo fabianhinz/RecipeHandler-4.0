757a1cabb62610661bd589e69d0232e40e4a69df
fabianhinz <hinzfabian.fh@gmail.com>
2020-01-11 18:50:09 +0100

    DeviceOrientationProvider

---
+++ src/Components/Provider/DeviceOrientationProvider.tsx
@@ -0,0 +1,69 @@
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    useMediaQuery,
+} from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/CloseTwoTone'
+import React, { FC, useContext, useEffect, useState } from 'react'
+
+import { SlideUp } from '../Shared/Transitions'
+import { useBreakpointsContext } from './BreakpointsProvider'
+
+interface Orientation {
+    portrait: boolean
+    landscape: boolean
+}
+
+const Context = React.createContext<Orientation | null>(null)
+
+export const useDeviceOrientationContext = () => useContext(Context) as Orientation
+
+const DeviceOrientationProvider: FC = ({ children }) => {
+    const [suggestionDialog, setSuggestionDialog] = useState(false)
+
+    const landscape = useMediaQuery('(orientation:landscape)')
+    const portrait = useMediaQuery('(orientation:portrait)')
+    const { isMobile } = useBreakpointsContext()
+
+    useEffect(() => {
+        if (landscape && isMobile)
+            (async () => {
+                try {
+                    await window.screen.orientation.lock('portrait')
+                } catch {
+                    // ? on unsupported devices fallback to a simple Dialog: https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/lock#Browser_compatibility
+                    setSuggestionDialog(true)
+                }
+            })()
+        if (portrait) setSuggestionDialog(false)
+    }, [landscape, portrait, isMobile])
+
+    return (
+        <>
+            <Context.Provider value={{ landscape, portrait }}>{children}</Context.Provider>
+            <Dialog TransitionComponent={SlideUp} open={suggestionDialog}>
+                <DialogContent>
+                    <DialogContentText>
+                        Auf Smartphones ist die Anzeige im Landscape-Modus nicht zu empfehlen
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Box flexGrow={1} display="flex" justifyContent="center">
+                        <Button
+                            startIcon={<CloseIcon />}
+                            onClick={() => setSuggestionDialog(false)}
+                            color="secondary">
+                            mir egal, ich weis was ich tue
+                        </Button>
+                    </Box>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
+
+export default DeviceOrientationProvider
