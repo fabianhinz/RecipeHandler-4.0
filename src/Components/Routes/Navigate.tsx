1a94ffeeaf70597d89d8d83efc5ba225c1db2962
fabianhinz <hinzfabian.fh@gmail.com>
2020-01-11 22:21:19 +0100

    fix ios fabs

--- src/Components/Routes/Navigate.tsx
+++ src/Components/Routes/Navigate.tsx
@@ -22,7 +22,7 @@ const useStyles = makeStyles(theme =>
             zIndex: theme.zIndex.drawer + 1,
             position: 'fixed',
             right: theme.spacing(2),
-            bottom: theme.spacing(4.5),
+            bottom: `calc(env(safe-area-inset-bottom) + ${theme.spacing(4.5)}px)`,
         },
     })
 )
