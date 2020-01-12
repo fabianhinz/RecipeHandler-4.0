df527b5299a2a9058b3c1119917e5d40ec5c7b9c
fabianhinz <hinzfabian.fh@gmail.com>
2020-01-12 13:39:52 +0100

    listen to orientationchange events instead of media queries

--- src/Components/Provider/DeviceOrientationProvider.tsx
+++ src/Components/Provider/DeviceOrientationProvider.tsx
@@ -1,11 +1,4 @@
-import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    useMediaQuery,
-} from '@material-ui/core'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText } from '@material-ui/core'
 import CloseIcon from '@material-ui/icons/CloseTwoTone'
 import React, { FC, useContext, useEffect, useState } from 'react'
 
@@ -22,27 +15,32 @@ const Context = React.createContext<Orientation | null>(null)
 export const useDeviceOrientationContext = () => useContext(Context) as Orientation
 
 const DeviceOrientationProvider: FC = ({ children }) => {
-    const [suggestionDialog, setSuggestionDialog] = useState(false)
-
-    const landscape = useMediaQuery('(orientation:landscape)')
     const { isMobile } = useBreakpointsContext()
+    const [suggestionDialog, setSuggestionDialog] = useState(
+        (window.orientation === 90 || window.orientation === -90) && isMobile
+    )
 
     useEffect(() => {
-        if (landscape && isMobile)
-            (async () => {
-                try {
-                    await window.screen.orientation.lock('portrait')
-                } catch {
-                    // ? on unsupported devices fallback to a simple Dialog: https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/lock#Browser_compatibility
-                    setSuggestionDialog(true)
-                }
-            })()
-        else setSuggestionDialog(false)
-    }, [landscape, isMobile])
+        const handleOrientationChange = () => {
+            if ((window.orientation === 90 || window.orientation === -90) && isMobile)
+                (async () => {
+                    try {
+                        await window.screen.orientation.lock('portrait')
+                    } catch {
+                        // ? on unsupported devices fallback to a simple Dialog: https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/lock#Browser_compatibility
+                        setSuggestionDialog(true)
+                    }
+                })()
+            else setSuggestionDialog(false)
+        }
+
+        window.addEventListener('orientationchange', handleOrientationChange)
+        return () => window.removeEventListener('orientationchange', handleOrientationChange)
+    }, [isMobile])
 
     return (
         <>
-            <Context.Provider value={{ landscape, portrait: !landscape }}>
+            <Context.Provider value={{ landscape: false, portrait: false }}>
                 {children}
             </Context.Provider>
             <Dialog TransitionComponent={SlideUp} open={suggestionDialog}>
