fd0bbe37224c9632d6e257820990ecf2cc012f84
fabianhinz <hinzfabian.fh@gmail.com>
2020-01-15 20:09:45 +0100

    ElementByIdService

--- src/hooks/useIntersectionObserver.tsx
+++ src/hooks/useIntersectionObserver.tsx
@@ -1,25 +1,32 @@
 import React, { useEffect } from 'react'
 
+import ElementIdService from '../services/elementIdService'
+
 interface useIntersectionObserverOptions {
     onIsIntersecting: () => void
 }
 
+const intersectionObserverId = ElementIdService.getId()
+
 const useIntersectionObserver = ({ onIsIntersecting }: useIntersectionObserverOptions) => {
     useEffect(() => {
-        const trigger = document.getElementById('intersection-observer-trigger')
+        const trigger = document.getElementById(intersectionObserverId)
         if (!trigger) return
 
-        const observer = new IntersectionObserver(entries => {
-            const [lastRecipeTrigger] = entries
-            if (lastRecipeTrigger.isIntersecting) onIsIntersecting()
-        })
+        const observer = new IntersectionObserver(
+            entries => {
+                const [lastRecipeTrigger] = entries
+                if (lastRecipeTrigger.isIntersecting) onIsIntersecting()
+            },
+            { threshold: 1, rootMargin: '50px' }
+        )
         observer.observe(trigger)
 
         return () => observer.unobserve(trigger)
     }, [onIsIntersecting])
 
     return {
-        IntersectionObserverTrigger: () => <div id="intersection-observer-trigger" />,
+        IntersectionObserverTrigger: () => <div id={intersectionObserverId} />,
     }
 }
 
