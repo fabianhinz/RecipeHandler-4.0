37aee7d2d6a12880eb2afa8043c24dea8b1a3589
fabianhinz <hinzfabian.fh@gmail.com>
2020-01-16 18:53:30 +0100

    ...v2

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,7 @@ const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
 module.exports = function override(config, env) {
     console.log('config-overrides')
     if (process.env.BUNDLE_ANALYZER_TOKEN) {
-        console.log('with token')
+        console.log('with token v2')
         config.plugins.push(
             new BundleAnalyzerPlugin({
                 token: process.env.BUNDLE_ANALYZER_TOKEN,
