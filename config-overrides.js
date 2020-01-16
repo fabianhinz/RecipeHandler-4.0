659a0a73f545f05dd5b4c6f3bf10e26cb2481a36
Fabian Hinz <fabianhinz@users.noreply.github.com>
2020-01-16 21:09:42 +0100

    fix version handling (#222)

--- config-overrides.js
+++ config-overrides.js
@@ -12,7 +12,7 @@ module.exports = function override(config, env) {
     config.plugins.push(
         new Webpack.DefinePlugin({
             __VERSION__: JSON.stringify(
-                process.env.VERSION || process.env.NODE_ENV === 'production' ? 'unkown' : 'dev'
+                process.env.VERSION || (process.env.NODE_ENV === 'production' ? 'unkown' : 'dev')
             ),
         })
     )
