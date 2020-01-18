b80cd650502bec556c46f17c24d5ec4a66a2dcd3
fabianhinz <hinzfabian.fh@gmail.com>
2020-01-18 10:43:46 +0100

    setUserId in analytics

--- src/Components/Provider/FirebaseAuthProvider.tsx
+++ src/Components/Provider/FirebaseAuthProvider.tsx
@@ -56,9 +56,12 @@ const FirebaseAuthProvider: FC = ({ children }) => {
             if (user.isAnonymous) {
                 setUser(null)
                 setLoginEnabled(true)
+                FirebaseService.analytics.setUserId(`Anonymous - ${user.uid}`)
             } else {
                 const userDocRef = FirebaseService.firestore.collection('users').doc(user.uid)
                 await userDocRef.update({ emailVerified: user.emailVerified })
+
+                FirebaseService.analytics.setUserId(user.uid)
 
                 userDocUnsubscribe = userDocRef.onSnapshot(doc => {
                     setUser({
