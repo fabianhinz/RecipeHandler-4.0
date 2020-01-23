7eea9031b0b26cf0579f7aeeaa6e86fc10bdc5c4
fabianhinz <hinzfabian.fh@gmail.com>
2020-01-23 23:15:54 +0100

    some experiments

--- src/Components/Shared/MarkdownInput.tsx
+++ src/Components/Shared/MarkdownInput.tsx
@@ -15,7 +15,7 @@ import FormatBoldIcon from '@material-ui/icons/FormatBold'
 import FormatItalicIcon from '@material-ui/icons/FormatItalic'
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted'
 import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered'
-import FormatUnderlinedIcon from '@material-ui/icons/FormatUnderlined'
+import FormatStrikethroughIcon from '@material-ui/icons/FormatStrikethrough'
 import TextFormatIcon from '@material-ui/icons/TextFormat'
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
 import React, { useState } from 'react'
@@ -59,32 +59,42 @@ interface Props {
     onChange: (value: string) => void
 }
 
-const HEADINGS = ['h1', 'h2', 'h3', 'h4']
+type Heading = 'h1' | 'h2' | 'h3' | 'h4'
+type List = 'bulletedList' | 'numberedList'
+type Text = 'bold' | 'italic' | 'strikethrough'
+type Format = Text | List | Heading
+
+const HEADINGS: Heading[] = ['h1', 'h2', 'h3', 'h4']
 
 const MarkdownInput = ({ defaultValue, onChange }: Props) => {
     const [value, setValue] = useState(defaultValue)
-    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null)
+    const [listFormat, setListFormat] = useState<List | null>(null)
+    const [selection, setSelection] = useState(0)
+    const [headingAnchorEl, setHeadingAnchorEl] = useState<HTMLButtonElement | null>(null)
 
     const classes = useStyles()
 
-    const handleFormat = (_event: React.MouseEvent<HTMLElement>, newFormats: string[]) => {
-        console.log(newFormats)
+    const handleFormat = (_event: React.MouseEvent<HTMLElement>, newFormat: Format) => {
+        console.log(newFormat)
         const selectedText = window.getSelection()?.toString()
-        console.log(selectedText)
+        if (selectedText) console.log(selectedText)
+
+        if (newFormat === 'bulletedList' || newFormat === 'numberedList')
+            setListFormat(prev => (prev === newFormat ? null : newFormat))
     }
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget)
+        setHeadingAnchorEl(event.currentTarget)
     }
 
     const handleClose = () => {
-        setAnchorEl(null)
+        setHeadingAnchorEl(null)
     }
 
     return (
         <div onBlur={() => onChange(value)}>
             <Paper elevation={0} className={classes.paper}>
-                <StyledToggleButtonGroup size="small" value="" onChange={handleFormat}>
+                <StyledToggleButtonGroup exclusive size="small" value="" onChange={handleFormat}>
                     <ToggleButton value="bold">
                         <FormatBoldIcon />
                     </ToggleButton>
@@ -92,13 +102,17 @@ const MarkdownInput = ({ defaultValue, onChange }: Props) => {
                         <FormatItalicIcon />
                     </ToggleButton>
                     <ToggleButton value="underlined">
-                        <FormatUnderlinedIcon />
+                        <FormatStrikethroughIcon />
                     </ToggleButton>
                 </StyledToggleButtonGroup>
 
                 <Divider className={classes.divider} orientation="vertical" />
 
-                <StyledToggleButtonGroup value="" size="small" onChange={handleFormat}>
+                <StyledToggleButtonGroup
+                    exclusive
+                    value={listFormat}
+                    size="small"
+                    onChange={handleFormat}>
                     <ToggleButton value="bulletedList">
                         <FormatListBulletedIcon />
                     </ToggleButton>
@@ -124,6 +138,19 @@ const MarkdownInput = ({ defaultValue, onChange }: Props) => {
                 value={value}
                 rows={15}
                 onChange={e => setValue(e.target.value)}
+                onKeyUp={e => {
+                    if (e.key === 'Enter' && listFormat === 'bulletedList') {
+                        const splittedInput = value.split('\n')
+                        const lastInput = splittedInput[splittedInput.length - 2]
+
+                        if (lastInput === '- ') {
+                            splittedInput.splice(-2, 2)
+                            splittedInput.push('')
+                            setValue(splittedInput.join('\n'))
+                        } else if (lastInput !== '') setValue(prev => `${prev}- `)
+                    }
+                }}
+                onBlur={e => setSelection(e.target.selectionStart as number)}
                 fullWidth
                 multiline
                 variant="outlined"
@@ -132,9 +159,9 @@ const MarkdownInput = ({ defaultValue, onChange }: Props) => {
 
             <Popover
                 classes={{ paper: classes.popoverPaper }}
-                id={anchorEl ? 'simple-popover' : undefined}
-                open={Boolean(anchorEl)}
-                anchorEl={anchorEl}
+                id={headingAnchorEl ? 'simple-popover' : undefined}
+                open={Boolean(headingAnchorEl)}
+                anchorEl={headingAnchorEl}
                 onClose={handleClose}
                 anchorOrigin={{
                     vertical: 'bottom',
@@ -150,8 +177,8 @@ const MarkdownInput = ({ defaultValue, onChange }: Props) => {
                             key={heading}
                             button
                             onClick={() => {
-                                setAnchorEl(null)
-                                handleFormat({} as any, [heading])
+                                setHeadingAnchorEl(null)
+                                handleFormat({} as any, heading)
                             }}>
                             <ListItemText primary={heading} />
                         </ListItem>
