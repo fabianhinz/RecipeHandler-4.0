6dad297cbe049a3f047368af1d6823df4c8c84e7
Fabian Hinz <hinzfabian.fh@gmail.com>
2020-02-10 18:08:29 +0100

    fix doppeltes "Haftung für Inhalte"

--- src/Components/Impressum/Impressum.tsx
+++ src/Components/Impressum/Impressum.tsx
@@ -154,22 +154,6 @@ const Impressum = () => {
                 </StyledCard>
             </Grid>
             <Grid item {...gridBreakpointProps}>
-                <StyledCard header="Haftung für Inhalte">
-                    <Typography>
-                        Als Diensteanbieter sind wir gemäß § 7 Abs. 1 TMG für eigene Inhalte auf
-                        diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10
-                        TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte
-                        oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu
-                        forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen
-                        zur Entfernung oder Sperrung der Nutzung von Informationen nach den
-                        allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung
-                        ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten
-                        Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden
-                        Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
-                    </Typography>
-                </StyledCard>
-            </Grid>
-            <Grid item {...gridBreakpointProps}>
                 <StyledCard header="Urheberrecht">
                     <Typography>
                         Die durch die Diensteanbieter, deren Mitarbeiter und beauftragte Dritte
