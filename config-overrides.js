58529e2a807521d81050f44f06037c5fbde132f2
Fabian Hinz <fabianhinz@users.noreply.github.com>
2020-02-11 13:40:12 +0100

    removed bundle analyzer (#330)

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,6 @@
-const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
 const Webpack = require('webpack')
 
 module.exports = function override(config, env) {
-    if (process.env.BUNDLE_ANALYZER_TOKEN) {
-        config.plugins.push(
-            new BundleAnalyzerPlugin({
-                token: process.env.BUNDLE_ANALYZER_TOKEN,
-            })
-        )
-    }
     config.plugins.push(
         new Webpack.DefinePlugin({
             __VERSION__: JSON.stringify(
