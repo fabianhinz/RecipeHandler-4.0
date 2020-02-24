beef8b74bb038940dcda63714947fb6e59446ce5
Fabian Hinz <hifa1013@hs-karlsruhe.de>
2020-02-24 23:32:36 +0100

    fix header name focus

--- src/Components/Recipe/Create/RecipeCreateHeader.tsx
+++ src/Components/Recipe/Create/RecipeCreateHeader.tsx
@@ -1,5 +1,5 @@
 import { createStyles, Grid, InputBase, makeStyles, Typography } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useRouteMatch } from 'react-router-dom'
 
 import { PATHS } from '../../Routes/Routes'
@@ -28,23 +28,17 @@ const RecipeCreateHeader = ({ inputDisabled, name, onNameChange }: Props) => {
     const classes = useStyles()
     const match = useRouteMatch()
 
-    const NameInput = () => (
-        <InputBase
-            disabled={inputDisabled}
-            classes={{ root: classes.inputBaseRoot, input: classes.inputBaseInput }}
-            value={value}
-            placeholder="Name"
-            onChange={e => setValue(e.target.value)}
-            onBlur={() => onNameChange(value)}
-        />
-    )
-
-    if (match.path === PATHS.recipeEdit()) return <NameInput />
-
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={match.path === PATHS.recipeCreate ? 2 : 0}>
             <Grid item xs={12}>
-                <NameInput />
+                <InputBase
+                    disabled={inputDisabled}
+                    classes={{ root: classes.inputBaseRoot, input: classes.inputBaseInput }}
+                    value={value}
+                    placeholder="Name"
+                    onChange={e => setValue(e.target.value)}
+                    onBlur={() => onNameChange(value)}
+                />
             </Grid>
             {match.path === PATHS.recipeCreate && (
                 <Grid item xs={12}>
