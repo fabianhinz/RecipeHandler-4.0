2a6bf704dc65aaffb945bb8d13b9c3066003f850
fabianhinz <hinzfabian.fh@gmail.com>
2021-02-28 12:30:00 +0100

    fix layers for responsive searchResults

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -7,9 +7,10 @@ import {
     InputBase,
     makeStyles,
     Paper,
+    Portal,
 } from '@material-ui/core'
 import { useSnackbar } from 'notistack'
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import useDebounce from '../../hooks/useDebounce'
@@ -25,6 +26,7 @@ import SearchResults from './SearchResults'
 
 interface StyleProps {
     focused: boolean
+    showResultsPaper: boolean
 }
 
 const useStyles = makeStyles(theme => ({
@@ -41,15 +43,21 @@ const useStyles = makeStyles(theme => ({
             theme.palette.type === 'light' && focused ? theme.shadows[1] : 'unset',
         borderRadius: BORDER_RADIUS,
         padding: theme.spacing(1),
-        transition: theme.transitions.create('background-color', {
+        transition: theme.transitions.create('all', {
             easing: theme.transitions.easing.easeOut,
         }),
+        borderBottomLeftRadius: (props: StyleProps) => (props.showResultsPaper ? 0 : BORDER_RADIUS),
+        borderBottomRightRadius: (props: StyleProps) =>
+            props.showResultsPaper ? 0 : BORDER_RADIUS,
     },
     searchResultsPaper: {
-        boxShadow: theme.shadows[4],
+        boxShadow: theme.palette.type === 'light' ? theme.shadows[1] : 'unset',
         backgroundColor: theme.palette.background.default,
+        borderTop: `1px solid ${theme.palette.divider}`,
         position: 'absolute',
-        top: 'calc(100% + 32px)',
+        top: '100%',
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
         left: 0,
         width: '100%',
         maxHeight: '60vh',
@@ -68,6 +76,10 @@ const useStyles = makeStyles(theme => ({
         paddingRight: theme.spacing(1),
         paddingLeft: theme.spacing(1),
     },
+    backdrop: {
+        zIndex: theme.zIndex.appBar - 1,
+        backdropFilter: 'blur(4px)',
+    },
 }))
 
 export const AlgoliaDocSearchRef = (
@@ -85,19 +97,30 @@ const Search = () => {
     const [value, setValue] = useState('')
 
     const history = useHistory()
-    const classes = useStyles({ focused })
     const debouncedValue = useDebounce(value, 500)
-
-    const { user } = useFirebaseAuthContext()
-    const { setError, setHits, error } = useSearchResultsContext()
     const { isTablet } = useBreakpointsContext()
-    const { enqueueSnackbar } = useSnackbar()
-
     const showResultsPaper = useMemo(() => focused && isTablet && debouncedValue.length > 0, [
         debouncedValue.length,
         focused,
         isTablet,
     ])
+    const classes = useStyles({ focused, showResultsPaper })
+
+    const { user } = useFirebaseAuthContext()
+    const { setError, setHits, error } = useSearchResultsContext()
+    const { enqueueSnackbar } = useSnackbar()
+
+    useLayoutEffect(() => {
+        const htmlEl = document.getElementsByTagName('html')[0]
+        const headerEl = document.getElementsByTagName('header')[0]
+        if (showResultsPaper) {
+            htmlEl.setAttribute('style', 'overflow:hidden; padding-right:16px')
+            headerEl.setAttribute('style', 'padding-right:16px;')
+        } else {
+            htmlEl.removeAttribute('style')
+            headerEl.removeAttribute('style')
+        }
+    }, [showResultsPaper])
 
     useEffect(() => {
         if (error) enqueueSnackbar('Fehler beim Abrufen der Daten', { variant: 'error' })
@@ -141,36 +164,38 @@ const Search = () => {
     }
 
     return (
-        <Container maxWidth="md" className={classes.container}>
-            <form onSubmit={handleSubmit} className={classes.searchContainer}>
-                <InputBase
-                    onFocus={handleFocusChange('in')}
-                    onBlur={handleFocusChange('out')}
-                    className={classes.searchInput}
-                    fullWidth
-                    placeholder="Suchen"
-                    value={value}
-                    onChange={handleInputChange}
-                    endAdornment={
-                        <Hidden xsDown>
-                            <InputAdornment position="end">{AlgoliaDocSearchRef}</InputAdornment>
-                        </Hidden>
-                    }
-                />
+        <>
+            <Container maxWidth="md" className={classes.container}>
+                <form onSubmit={handleSubmit} className={classes.searchContainer}>
+                    <InputBase
+                        onFocus={handleFocusChange('in')}
+                        onBlur={handleFocusChange('out')}
+                        className={classes.searchInput}
+                        fullWidth
+                        placeholder="Suchen"
+                        value={value}
+                        onChange={handleInputChange}
+                        endAdornment={
+                            <Hidden xsDown>
+                                <InputAdornment position="end">
+                                    {AlgoliaDocSearchRef}
+                                </InputAdornment>
+                            </Hidden>
+                        }
+                    />
 
-                <Grow in={showResultsPaper} mountOnEnter>
-                    <Paper className={classes.searchResultsPaper}>
-                        <SearchResults />
-                    </Paper>
-                </Grow>
-                <Backdrop
-                    open={showResultsPaper}
-                    style={{
-                        backdropFilter: 'blur(4px)',
-                    }}
-                />
-            </form>
-        </Container>
+                    <Grow in={showResultsPaper} mountOnEnter>
+                        <Paper className={classes.searchResultsPaper}>
+                            <SearchResults />
+                        </Paper>
+                    </Grow>
+                </form>
+            </Container>
+
+            <Portal>
+                <Backdrop id="whereami" className={classes.backdrop} open={showResultsPaper} />
+            </Portal>
+        </>
     )
 }
 
