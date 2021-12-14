217717dedb1519291b7b794deff9d45a979f050d
Fabian Hinz <fabianhinz@users.noreply.github.com>
2021-12-14 21:08:11 +0100

    Kontextmenü für die Startseite (#1200)
    * HomeRecipeContextMenu component
    
    * nvm
    
    * ContextMenu v2

---
+++ src/Components/Home/HomeRecipeContextMenu.tsx
@@ -0,0 +1,77 @@
+import {
+    Divider,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    makeStyles,
+    Popover,
+    PopoverPosition,
+    useTheme,
+} from '@material-ui/core'
+import { AddCircleOutlined } from '@material-ui/icons'
+import { ReactNode, useState } from 'react'
+
+import { Recipe } from '../../model/model'
+import RecipeBookmarkButton from '../Recipe/RecipeBookmarkButton'
+import RecipeShareButton from '../Recipe/RecipeShareButton'
+import { PATHS } from '../Routes/Routes'
+
+const useStyles = makeStyles(theme => ({
+    homeRecipeContextMenuRoot: {
+        display: 'flex',
+    },
+}))
+
+interface Props {
+    children: ReactNode
+    name: Recipe['name']
+    numberOfComments?: Recipe['numberOfAttachments']
+}
+
+const HomeRecipeContextMenu = (props: Props) => {
+    const [anchorPosition, setAnchorPosition] = useState<PopoverPosition | undefined>()
+    const classes = useStyles()
+    const theme = useTheme()
+
+    return (
+        <>
+            <Popover
+                PaperProps={{ className: classes.homeRecipeContextMenuRoot }}
+                transitionDuration={{ enter: theme.transitions.duration.complex, exit: 0 }}
+                anchorReference="anchorPosition"
+                anchorPosition={anchorPosition}
+                open={Boolean(anchorPosition)}
+                onClose={() => setAnchorPosition(undefined)}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}>
+                <List disablePadding onClick={() => setAnchorPosition(undefined)}>
+                    <ListItem onClick={() => window.open(PATHS.details(props.name))} button>
+                        <ListItemIcon>
+                            <AddCircleOutlined />
+                        </ListItemIcon>
+                        <ListItemText primary="In neuem Tab öffnen" />
+                    </ListItem>
+                    <Divider />
+
+                    <RecipeBookmarkButton variant="ListItem" name={props.name} />
+                    <RecipeShareButton variant="ListItem" name={props.name} />
+                </List>
+            </Popover>
+            <div
+                onContextMenu={e => {
+                    setAnchorPosition({ top: e.clientY, left: e.clientX })
+                }}>
+                {props.children}
+            </div>
+        </>
+    )
+}
+
+export default HomeRecipeContextMenu
