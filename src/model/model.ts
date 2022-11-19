098e05875b756ec8457faae7848676ddb65b110c
fabianhinz <hinzfabian.fh@gmail.com>
2022-11-19 18:46:51 +0100

    fix build

--- src/model/model.ts
+++ src/model/model.ts
@@ -202,3 +202,8 @@ export interface Expense {
 export type ArchivedExpense = Expense & { deletedAt: firebase.default.firestore.Timestamp }
 
 export type Nullable<UnderlyingType> = null | UnderlyingType
+
+export type ChangesRecord<DocType> = Record<
+  firebase.default.firestore.DocumentChangeType,
+  Map<DocumentId, DocType>
+>
